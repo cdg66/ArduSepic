84c30c99d6fd53249ee15d659f115b1cc723781f
cdg66 <claudecdg66@icloud.com>
2022-07-22 19:37:06 -0400

    add pid example and pin definition

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <PID_v1.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define PIN_V_OUTPUT 1
+#define PIN_I_OUTPUT 2
+
+#define PIN_V_INPUT 3
+#define PIN_I_INPUT 4
+
+#define PIN_T_INPUT 5
+
+#define PIN_MOSFET 6
+
+
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=2, Ki=5, Kd=1;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+void setup()
+{
+  //initialize the variables we're linked to
+  Input = analogRead(PIN_V_INPUT);
+  Setpoint = 100;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Input = analogRead(PIN_V_INPUT);
+  myPID.Compute();
+  analogWrite(PIN_MOSFET, Output);
 }
